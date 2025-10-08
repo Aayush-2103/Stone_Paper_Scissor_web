94a6c941f737f711b0a9b93d1473be603cc1770b
Aayush-2103 <2103aayush@gmail.com>
2025-10-08 15:58:50 +0530

    final comit

---
+++ script.js
@@ -0,0 +1,35 @@
+let choices = document.querySelectorAll('#choice');
+let userScore = 0;
+let compScore = 0;
+let user_score = document.querySelector('#user_score');
+let comp_score = document.querySelector('#computer_score');
+let your_choice = document.querySelector('#yourChoice');
+let comp_choice = document.querySelector('#compChoice');
+
+
+const playGame=(userChoice, compChoice)=>{
+    if ((userChoice==='rock' && compChoice==='scissors') || (userChoice==='paper' && compChoice==='rock') || (userChoice==='scissors' && compChoice==='paper')){
+        userScore ++
+    } else if (userChoice!=compChoice){
+        compScore ++
+    }
+};
+
+const random_choice = () => {
+    let options = ["rock", "paper", "scissors"];
+    let index = Math.floor(Math.random()*3);
+    return options[index];
+};
+
+choices.forEach((choice)=>{
+    choice.addEventListener('click',()=>{
+        const user_choice = choice.getAttribute('name');
+        let computer_choice = random_choice();
+        your_choice.innerText = user_choice[0].toUpperCase() + user_choice.slice(1);
+        comp_choice.innerText = computer_choice[0].toUpperCase() + computer_choice.slice(1);
+        playGame(user_choice, computer_choice);
+        user_score.innerText = userScore;
+        comp_score.innerText = compScore;
+    });
+});
+
